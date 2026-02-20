f731a19593c3444c04d82ce4b16f161faab83ec5
DIPEN-Dp <yourgithubemail@gmail.com>
2026-02-20 14:55:14 +0530

    Error Fixed

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
@@ -7,7 +8,7 @@ function layout(){
 
     return(
         <>
-        <Home />
+        <Header />
         <Outlet />
         <Footer />
         </>
