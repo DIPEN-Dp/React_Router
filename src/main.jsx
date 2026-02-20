7397446d87fb3e747347cfd2596a1ee996ab8237
DIPEN-Dp <yourgithubemail@gmail.com>
2026-02-20 15:39:55 +0530

    Added contact component to routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,29 +2,48 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import layout from './Layout.jsx'
 
-const Router = createBrowserRouter([
-         {
-          path:"/",
-          element:<Layout/>,
-          children:[
-            {
-              path:"",
-              element:<Home/>
-            },
-            {
-              path:"About",
-              element:<About/>
-            }
-          ]
-      }
-])
+// Method:-1 to Create browser router for routing elements
+// const Router = createBrowserRouter([
+//            {
+//               path:"/",
+//               element:<Layout/>,
+//               children:[
+//                   {
+//                       path:"",
+//                       element:<Home/>
+//                     },
+//                     {
+//                         path:"About",
+//                         element:<About/>
+//                       },
+//                       {
+//                           path:"Contact",
+//                           element:<Contact/>
+//                         }
+//                       ]
+//              }
+//             ])
+          
+  //Method:-2 to Create browser router for routing elements
 
-createRoot(document.getElementById('root')).render(
+    const Router=createBrowserRouter(
+      createRoutesFromElements(
+        <Route path='/' element={<Layout/>}>
+        <Route path='' element={<Home/>}/>
+        <Route path='About' element={<About/>}/>
+        <Route path='Contact' element={<Contact/>}/>
+      </Route>
+      )
+    )
+            
+            createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={Router}/>
   </StrictMode>,
